7e9dacee25e4f4a231012f0799f1621f2c52083a
Osama Abdu Ahmed Algumaei <94931636+OsamaAboAdel@users.noreply.github.com>
2022-03-07 23:10:53 +0300

    Update R_3_7.java

--- R_3_7.java
+++ R_3_7.java
@@ -1,8 +1,7 @@
 package Assignments.Lab4;
-
 /**
- * Created by Ahmed Al masadi on 2/02/2022.
- * */
+ * Created by ${*  Osama Abo Adel  *} on 1/2/2022.
+ */
 public class R_3_7 {
     public void addFirst(E ele){
         if (isEmpty()){
